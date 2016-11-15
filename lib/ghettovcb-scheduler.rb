1f83440dbc13988bc0a6b2bbe3defd4773c77aef
Nicolas Jolet <nicolas.jolet+github@gmail.com>
2016-11-15 18:43:32 +0100

    added logger

--- lib/ghettovcb-scheduler.rb
+++ lib/ghettovcb-scheduler.rb
@@ -2,8 +2,14 @@ $LOAD_PATH << __dir__
 
 require 'ghettovcb-scheduler/config'
 require 'ghettovcb-scheduler/scheduler'
+require 'logger'
+
+logger = Logger.new(STDOUT)
 
 config = Config::load(File.join(__dir__, '../config/config.yaml'))
+logger.level = Logger.const_get config.log_level
+
+logger.debug(config.inspect)
 
 scheduler = Scheduler.new(config.tasks, config.default_user)
 
